8eca4323b4d0ef4e1a1372756159e88cec746cba
Yinuo Liu <77820812+YinuoLiu0708@users.noreply.github.com>
2023-03-20 19:50:41 -0400

    first commit

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppTitleStrategy } from './app-title.strategy';
+import { GateComponent } from './gate/gate.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileEditorComponent } from './profile/profile-editor/profile-editor.component';
+
+
+const routes: Routes = [
+  HomeComponent.Route,
+  ProfileEditorComponent.Route,
+  GateComponent.Route,
+  { path: 'admin', title: 'Admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled'
+  })],
+  exports: [RouterModule],
+  providers: [AppTitleStrategy.Provider]
+})
+export class AppRoutingModule {}
