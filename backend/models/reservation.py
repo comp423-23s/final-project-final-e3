3c28de7718d180a064ec2a58950b59f053e6eb45
Tianyi Niu <tianyin4@email.unc.edu>
2023-03-28 21:45:33 -0400

    Add data model's import statements

--- backend/models/reservation.py
+++ backend/models/reservation.py
@@ -1,6 +1,8 @@
 """Reservation model representing reservation required by users"""
 
 from pydantic import BaseModel
+from datetime import datetime
+from . import Room, Equipment, User
 
 class Reservation(BaseModel):
     subject: Room | Equipment
