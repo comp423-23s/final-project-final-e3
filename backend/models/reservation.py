d5ccbd101a5d0396005b7f3a33cae57c960eb59c
Tianyi Niu <tianyin4@email.unc.edu>
2023-03-29 13:08:16 -0400

    Add ReservationRequest data model; Add new reservation POST route

--- backend/models/reservation.py
+++ backend/models/reservation.py
@@ -4,6 +4,12 @@ from pydantic import BaseModel
 from datetime import datetime
 from . import Room, Equipment, User
 
+class ReservationRequest(BaseModel):
+    pid: int
+    start: str
+    end: str
+    subject_name: str
+
 class Reservation(BaseModel):
     id: int | None = None
     subject: Room | Equipment
