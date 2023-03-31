2f0df77067ba122893bbabcfea1cc0d1f0778bb1
Tianyi Niu <tianyin4@email.unc.edu>
2023-03-31 02:03:00 -0400

    Fix list Rooms functionality, add __init__.py to reset_database dir
    To run reset_database.py:
    pwd: workspace
    run: "python3 -m backend.scripts.reset_database"

--- backend/script/dev_data/rooms.py
+++ backend/script/dev_data/rooms.py
@@ -1,6 +1,11 @@
 """Sample rooms"""
 
-from ..models import Room
+from ...models import Room
 
 room1 = Room(id=1, name="A1", max_capacity=3)
 room2 = Room(id=2, name="A2", max_capacity=5)
+
+models = [
+    room1, 
+    room2
+]
--- backend/script/reset_database.py
+++ backend/script/reset_database.py
@@ -66,6 +66,7 @@ with Session(engine) as session:
 with Session(engine) as session:
     from ..entities import RoomEntity
     from .dev_data import rooms
+    to_entity = entities.RoomEntity.from_model
     session.add_all([to_entity(model) for model in rooms.models])
     session.execute(text(f'ALTER SEQUENCE {entities.RoomEntity.__table__}_id_seq RESTART WITH {len(rooms.models) + 1}'))
     session.commit()
